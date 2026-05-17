e36ea6b8ded3769c50f71aab4937dcedb3508849
Maoz-Daniel <maoz2231@gmail.com>
2026-05-17 11:14:09 +0300

    final

--- client/src/hooks/usePaginatedItems.js
+++ client/src/hooks/usePaginatedItems.js
@@ -26,8 +26,8 @@ function usePaginatedItems(initialItems, initialNextPage) {
     try {
       const pageData = await fetchPage(nextPage)
 
-      setItems((currentItems) => mergeItems(currentItems, pageData.data))
-      setNextPage(pageData.next)
+      setItems((currentItems) => mergeItems(currentItems, pageData.data)) // Update nextPage based on the response from fetchPage
+      setNextPage(pageData.next) // Return the page data so the caller can use it if needed, to check if there are more pages
 
       return pageData
     } finally {
